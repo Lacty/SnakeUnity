4f370f6325833c547cf99b860bd29776bb673a87
Lacty <akira206@gmail.com>
2016-07-29 16:53:26 +0900

    add project

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+namespace Assets.Scripts {
+  public enum SnakeType {
+    Head, Body
+  }
+
+  public class Snake : MonoBehaviour {
+    private SnakeType _type;
+    public SnakeType Type {
+      get { return _type; }
+      set { _type = value; }
+    }
+  }
+}
---
+++ Assets/Scripts/SnakeStage.cs
@@ -0,0 +1,343 @@
+﻿
+using UnityEngine;
+using System.Collections.Generic;
+
+using Assets.Scripts;
+
+
+public class SnakeStage : MonoBehaviour {
+
+  [SerializeField]
+  private GameObject _cell = null;
+  public GameObject Cell {
+    get
+    {
+      if (_cell != null) return _cell;
+      _cell = Resources.Load<GameObject>("Cell/Cell");
+      return _cell;
+    }
+  }
+
+  [SerializeField]
+  private GameObject _snakeHead = null;
+  public GameObject SnakeHead {
+    get
+    {
+      if (_snakeHead != null) return _snakeHead;
+      _snakeHead = Resources.Load<GameObject>("Snake/Head");
+      return _snakeHead;
+    }
+  }
+
+  [SerializeField]
+  private GameObject _snakeBody = null;
+  public GameObject SnakeBody {
+    get
+    {
+      if (_snakeBody != null) return _snakeBody;
+      _snakeBody = Resources.Load<GameObject>("Snake/Body");
+      return _snakeBody;
+    }
+  }
+
+  [SerializeField]
+  private GameObject _feed = null;
+  public GameObject Feed {
+    get
+    {
+      if (_feed != null) return _feed;
+      _feed = Resources.Load<GameObject>("Feed/Feed");
+      return _feed;
+    }
+  }
+
+  [SerializeField]
+  private float UpdateRate = 0.3f;
+  private float _updateCount = 0.0f;
+
+  [SerializeField]
+  private int NumOfGrow = 3;
+
+  private const int Columns = 10;
+  private const int Rows = 10;
+
+  private readonly GameObject[,] _cells = new GameObject[Rows, Columns];
+
+  private Vector3 _nextDir = new Vector3(0, -1, 0);
+  // 配列だが複数形でないので注意
+  private List<GameObject> _snake = new List<GameObject>();
+
+  private GameObject _lightGameObject;
+  private bool _isGameOver = false;
+
+  private int _score = 0;
+
+  void Start()
+  {
+    InitGame();
+  }
+
+  void Update()
+  {
+    UpdateGame();
+  }
+
+  void OnGUI()
+  {
+    if (!_isGameOver) return;
+    int width = 100;
+    int height = 60;
+    float halfWidth = width * 0.5f;
+    float halfHeight = height * 0.5f;
+    float halfWindowWidth = Screen.width * 0.5f;
+    float halfWindowHeight = Screen.height * 0.5f;
+    var rect = new Rect(halfWindowWidth - halfWidth,
+                        halfWindowHeight - halfHeight,
+                        width, height);
+    if (GUI.Button(rect, _score + "Eat! Restart"))
+    {
+      ResetGame();
+    }
+  }
+
+  private void UpdateGame()
+  {
+    if (_isGameOver) return;
+    UpdateDirection();
+    if (!ShouldUpdate()) return;
+    MoveSnake();
+    EatFeed();
+    if (!IsEatMySelf()) return;
+    StartCoroutine(Diffusion());
+    _isGameOver = true;
+  }
+
+  private IEnumerator<GameObject> Diffusion()
+  {
+    List<Vector3> poss = new List<Vector3>();
+    foreach (var snake in _snake)
+    {
+      poss.Add(new Vector3(Random.Range(-Rows * 2, Rows * 2),
+                           Random.Range(-Columns * 2, Columns * 2),
+                           Random.Range(-Rows * 2, Rows * 2)));
+    }
+
+    for (int i = 0; i < 20; i++)
+    {
+      int k = 0;
+      foreach (var snake in _snake)
+      {
+        var dir = snake.transform.position - poss[k++];
+        snake.transform.position -= dir *= 0.06f;
+      }
+      yield return null;
+    }
+    foreach (var snake in _snake)
+    {
+      DestroyObject(snake);
+    }
+    _snake.Clear();
+  }
+
+  private bool IsEatMySelf()
+  {
+    // 4番目以降でないと体に当たることはないので３から回す
+    for (int i = 3; i < _snake.Count; i++)
+    {
+      if (_snake[0].transform.position == _snake[i].transform.position) return true;
+    }
+    return false;
+  }
+
+  private void EatFeed()
+  {
+    if (_snake[0].transform.position.x == _feed.transform.position.x)
+    {
+      if (_snake[0].transform.position.y == _feed.transform.position.y)
+      {
+        GrowUp(NumOfGrow);
+        MoveFeed();
+        _score++;
+      }
+    }
+  }
+
+  private void MoveFeed()
+  {
+    var list = GetPutablePos();
+    _feed.transform.position = list[Random.Range(0, list.Count)];
+  }
+
+  private List<Vector3> GetPutablePos()
+  {
+    List<Vector3> sum = new List<Vector3>();
+    for (int r = 0; r < Rows; r++)
+    {
+      for (int c = 0; c < Columns; c++)
+      {
+        sum.Add(new Vector3(r, c, 0));
+      }
+    }
+    foreach (var snake in _snake)
+    {
+      sum.Remove(new Vector3(snake.transform.position.x, snake.transform.position.y, 0));
+    }
+    return sum;
+  }
+
+  private void MoveSnake()
+  {
+    for (int i = _snake.Count - 1; i > 0; i--)
+    {
+      _snake[i].transform.position = _snake[i - 1].transform.position;
+    }
+    _snake[0].transform.Translate(_nextDir);
+    LoopMove();
+  }
+
+  private void LoopMove()
+  {
+    if (_snake[0].transform.position.x >= Rows) _snake[0].transform.Translate(-Rows, 0, 0);
+    if (_snake[0].transform.position.x < 0) _snake[0].transform.Translate(Rows, 0, 0);
+    if (_snake[0].transform.position.y >= Columns) _snake[0].transform.Translate(0, -Columns, 0);
+    if (_snake[0].transform.position.y < 0) _snake[0].transform.Translate(0, Columns, 0);
+  }
+
+  private void GrowUp(int num)
+  {
+    var pos = _snake[_snake.Count - 1].transform.position;
+    for (int i = 0; i < num; i++)
+    {
+      _snake.Add(CreateSnakeBody((int)pos.x, (int)pos.y));
+    }
+  }
+
+  private void UpdateDirection()
+  {
+    if (IsValidInput(KeyCode.W)) _nextDir.Set(0, 1, 0);
+    else if (IsValidInput(KeyCode.S)) _nextDir.Set(0, -1, 0);
+    else if (IsValidInput(KeyCode.A)) _nextDir.Set(-1, 0, 0);
+    else if (IsValidInput(KeyCode.D)) _nextDir.Set(1, 0, 0);
+  }
+
+  private bool IsValidInput(KeyCode key)
+  {
+    switch (key)
+    {
+      case KeyCode.W:
+      if (Input.GetKeyDown(KeyCode.W))
+        if (GetDirection(0).y != -1) { return true; }
+      break;
+      case KeyCode.S:
+      if (Input.GetKeyDown(KeyCode.S))
+        if (GetDirection(0).y != 1) { return true; }
+      break;
+      case KeyCode.A:
+      if (Input.GetKeyDown(KeyCode.A))
+        if (GetDirection(0).x != 1) { return true; }
+      break;
+      case KeyCode.D:
+      if (Input.GetKeyDown(KeyCode.D))
+        if (GetDirection(0).x != -1) { return true; }
+      break;
+    }
+    return false;
+  }
+
+  private Vector3 GetDirection(int index)
+  {
+    return _snake[index].transform.position
+         - _snake[index + 1].transform.position;
+  }
+
+  private bool ShouldUpdate()
+  {
+    _updateCount += Time.deltaTime;
+    if (_updateCount <= UpdateRate) return false;
+    _updateCount -= UpdateRate;
+    return true;
+  }
+
+  private void InitGame()
+  {
+    _isGameOver = false;
+    InitCells();
+    InitSnake();
+    _feed = CreateFeed(5, 5);
+    CreateLight();
+  }
+
+  private void ResetGame()
+  {
+    _isGameOver = false;
+    _score = 0;
+    InitSnake();
+  }
+
+  private void CreateLight()
+  {
+    _lightGameObject = new GameObject("Directional Light");
+    _lightGameObject.AddComponent<Light>();
+    _lightGameObject.GetComponent<Light>().type = LightType.Directional;
+    _lightGameObject.GetComponent<Light>().color = new Color(1, 0.96f, 0.86f, 1);
+    _lightGameObject.transform.Rotate(50, 330, 0);
+    _lightGameObject.transform.position = new Vector3(0, 3, 0);
+  }
+
+  private void InitCells()
+  {
+    for (var r = 0; r < Rows; r++)
+    {
+      for (var c = 0; c < Columns; c++)
+      {
+        _cells[r, c] = CreateCell(r, c);
+      }
+    }
+  }
+
+  private void InitSnake()
+  {
+    foreach (var snake in _snake)
+    {
+      DestroyObject(snake);
+    }
+    SnakeHead.GetComponent<Snake>().Type = SnakeType.Head;
+    SnakeBody.GetComponent<Snake>().Type = SnakeType.Body;
+    _snake.Clear();
+    _snake.Add(CreateSnakeHead(0, Columns - 4));
+    _snake.Add(CreateSnakeBody(0, Columns - 3));
+    _snake.Add(CreateSnakeBody(0, Columns - 2));
+    _snake.Add(CreateSnakeBody(0, Columns - 1));
+
+    _nextDir.Set(0, -1, 0);
+  }
+
+  private GameObject CreateObject(GameObject origin, int r, int c)
+  {
+    var gameObj = Instantiate(origin);
+    gameObj.name = origin.name + "(" + r + "," + c + ")";
+    gameObj.transform.Translate(r, c, 0);
+    gameObj.transform.parent = gameObject.transform;
+    return gameObj;
+  }
+
+  private GameObject CreateCell(int r, int c)
+  {
+    return CreateObject(Cell, r, c);
+  }
+
+  private GameObject CreateSnakeHead(int r, int c)
+  {
+    return CreateObject(SnakeHead, r, c);
+  }
+
+  private GameObject CreateSnakeBody(int r, int c)
+  {
+    return CreateObject(SnakeBody, r, c);
+  }
+
+  private GameObject CreateFeed(int r, int c)
+  {
+    return CreateObject(Feed, r, c);
+  }
+}
